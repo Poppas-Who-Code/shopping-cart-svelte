6b487904c49cc23a881f268d26322d2a63ebb75e
Steven Balasta <sbal13@gmail.com>
2025-02-14 21:43:24 -0500

    done

--- src/lib/components/card.svelte
+++ src/lib/components/card.svelte
@@ -8,9 +8,16 @@
 	export let color: string;
 	export let buttonText: string;
 	export let count: number | undefined = undefined;
+
+	let showDetails = false;
 </script>
 
-<div class="flex relative flex-col border border-gray-900 p-2 rounded-md">
+<div class="relative flex flex-col border border-gray-900 p-2 rounded-md">
+	{#if showDetails}
+		<button on:click={() => (showDetails = false)}>
+			<div class="absolute size-[90%] bg-yellow-300">SOMETHING</div>
+		</button>
+	{/if}
 	{#if count}
 		<div
 			class="absolute right-[-12px] top-[-13px] py-1 px-2 border border-gray-900 rounded-full bg-blue-800 text-white text-xs font-semibold"
@@ -18,7 +25,9 @@
 			{count}
 		</div>
 	{/if}
-	<img src={imageSrc} class="max-w-[200px] h-full border border-gray-400" alt={name} />
+	<button on:click={() => (showDetails = true)}>
+		<img src={imageSrc} class="max-w-[200px] h-full border border-gray-400" alt={name} />
+	</button>
 	<div class="flex justify-between items-end grow p-2">
 		<span>{name}:</span>
 		<span>${price}</span>
